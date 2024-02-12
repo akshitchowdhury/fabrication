711fc152ac2da16cba8bbd4bf65c8144f73f42a0
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-02-12 14:33:49 +0530

    f5

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 
 import Card from './Card/Card'
-
+import './Gallery.css'
 const Gallery = () => {
   return (
     <div className='main-gallery'>
     <h1>Our Gallery</h1>
     <br />
-    <div className='flex flex-wrap justify-center'>
+    <div className= 'flex flex-wrap justify-center'>
     <Card/>
-    <Card/>
-    <Card/>
+    
+    
     </div>
     </div>
   )
