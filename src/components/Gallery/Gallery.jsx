bcc06e6be06f47a971069b8b074f244f570e83f7
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-02-12 14:08:15 +0530

    f4

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -5,11 +5,14 @@ import Card from './Card/Card'
 
 const Gallery = () => {
   return (
-    <div className='gallery'>
+    <div className='main-gallery'>
     <h1>Our Gallery</h1>
     <br />
+    <div className='flex flex-wrap justify-center'>
     <Card/>
-       
+    <Card/>
+    <Card/>
+    </div>
     </div>
   )
 }
