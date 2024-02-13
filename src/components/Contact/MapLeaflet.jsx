3f6836d621af652eabe4adfadba3ac4fe7da9920
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-02-13 13:11:02 +0530

    s3

--- src/components/Contact/MapLeaflet.jsx
+++ src/components/Contact/MapLeaflet.jsx
@@ -5,18 +5,20 @@ import './MapLeaflet.css'; // Create a separate CSS file for styling
 import 'tailwindcss/tailwind.css'; // Import Tailwind CSS
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+
 const MapLeaflet = () => {
   const [position, setPosition] = useState([12.8985, 77.6058]); // Default position
   const [userLocation, setUserLocation] = useState(null);
 
   const latitude = 12 + (53 / 60) + (24.3 / 3600);
-const longitude = 77 + (33 / 60) + (42.8 / 3600);
+  const longitude = 77 + (33 / 60) + (42.8 / 3600);
 
-const redIcon = new L.Icon({
-  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-});
+  const redIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+  });
+
   useEffect(() => {
     // Get user's geolocation
     navigator.geolocation.getCurrentPosition(
@@ -31,13 +33,9 @@ const redIcon = new L.Icon({
   }, []);
 
   return (
-    <div className="w-full h-screen flex justify-end"> {/* Tailwind CSS responsive container */}
-      <div className="custom-map-container lg:w-3/4 p-4 lg:h-3/4 rounded-lg shadow-md bg-white">  {/* Custom width and height */}
-        <MapContainer
-          center={position}
-          zoom={13}
-          className="w-full h-full"
-        >
+    <div className="w-full h-screen flex justify-center items-center">
+      <div className="w-full md:w-3/4 lg:w-3/4 xl:w-1/3 h-96">
+        <MapContainer center={position} zoom={13} className="w-full h-full">
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
