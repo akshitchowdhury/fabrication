445e3bfb7a62c5b6f12cd52e7518f5950093fbdf
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-02-14 16:10:46 +0530

    bt0

---
+++ src/components/NavBlock/BgText/BgText.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './BgText.css'
+const BgText = () => {
+  return (
+    <div>
+        <h3>Welcome to KK Fabrication Industries</h3>
+    </div>
+  )
+}
+
+export default BgText
