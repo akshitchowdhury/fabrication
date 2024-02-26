12b0b51090935b6d05c2a4543532f268ea9832d4
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-02-26 13:11:31 +0530

    final_touches2

--- src/components/About Us/AboutUs.jsx
+++ src/components/About Us/AboutUs.jsx
@@ -44,7 +44,7 @@ const AboutUs = forwardRef((props, ref) => {
               <img src={about2} alt="Service Image" className=" rounded-md shadow-md" />
             </div>
             <p className="text-lg leading-relaxed">
-            Welcome to KK Industries, where precision meets purpose in fabrication and construction solutions. With expertise honed over many years, we're not just building structures; we're crafting communities. From intricate fabrication projects to the construction of essential labor camps, we're committed to excellence at every turn. Trust us to bring your vision to life with quality, efficiency, and a dedication to making a lasting impact. At KK Industries, we're not just building; we're shaping futures. Join us in building a better tomorrow, one project at a time.
+            Welcome to KK Industries, where precision meets purpose in fabrication and construction solutions. With expertise honed over many years, we're not just building structures; we're crafting communities. From intricate fabrication projects to the construction of essential labor camps, we're committed to excellence at every turn. Trust us to bring your vision to life with quality, efficiency, and a dedication to making a lasting impact. At KK Industries, we're not just building; we're shaping futures. Join us in building a better tomorrow, multiple projects at a time.
             </p>
           </div>
           <div className="w-full md:w-1/2 h-auto" >
